0d1de2f28a8e43151a13194000e3d8a39a056cc1
kirinchen <a29243685@msn.com>
2017-09-22 12:43:13 +0800

    new Obj

---
+++ FireObj.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace RFNEet.firebase {
+    public abstract class FireObj : MonoBehaviour {
+
+        private FireNode node;
+        public string pid;
+        public string oid;
+
+        void initAtFire(FireObjCenter.InitBundle ib) {
+            node = ib.node;
+            setData(ib.data);
+            node.addValueChangedListener(onValueChnaged);
+        }
+
+        private void setData(RemoteData data) {
+            pid = data.pid;
+            oid = data.oid;
+            onInit(data);
+        }
+
+        public void init(string p,string o) {
+            if (node != null) return;
+            pid = p;
+            oid = o;
+            node = FirebaseManager.getRepo().get(pid, oid);
+            node.post(getCurrentData());
+            node.addValueChangedListener(onValueChnaged);
+        }
+
+        internal abstract RemoteData getCurrentData();
+        internal abstract void onValueChnaged(RemoteData obj);
+        internal abstract void onInit(RemoteData d);
+    }
+}
---
+++ FirePlayerCenter.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace RFNEet.firebase {
+    public class FirePlayerCenter : MonoBehaviour {
+
+
+    }
+}
